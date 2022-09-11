f3426efa77d2b38f69c0b30c0942a8a758ee89b1
Emmanuel Tamburini <emmanueltamburini@gmail.com>
2022-09-10 22:21:08 -0400

    number pipe

--- src/app/sell/pages/numbers/numbers.component.ts
+++ src/app/sell/pages/numbers/numbers.component.ts
@@ -1,14 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-numbers',
   templateUrl: './numbers.component.html'
 })
-export class NumbersComponent implements OnInit {
+export class NumbersComponent {
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+  public bigNumber: number = 245882.21712;
+  public percentage: number = 0.47;
 
 }
