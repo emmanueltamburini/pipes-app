d7715021ad130f7ee4ff038ff7df724dc1d04add
Emmanuel Tamburini <emmanueltamburini@gmail.com>
2022-09-10 23:45:21 -0400

    i18n pipes has been done

--- src/app/sell/pages/no-commons/no-commons.component.ts
+++ src/app/sell/pages/no-commons/no-commons.component.ts
@@ -7,19 +7,34 @@ import { Component } from '@angular/core';
 export class NoCommonsComponent {
   // i18nSelect
   public name: string = 'Emmanuel';
-  public gender: string = 'male'
+  public gender: string = 'male';
 
   public mapPronouns: { [key: string]: string } = {
     'female': 'she',
     'male': 'he'
+  };
+
+  public changePerson(): void {
+    if (this.gender === 'male') {
+      this.name = 'Ana';
+      this.gender = 'female';
+    } else {
+      this.name = 'Emmanuel';
+      this.gender = 'male';
+    }
   }
 
-    // i18nPlural
-    public clients: string [] = ['Ana', 'Pedro', 'Diana'];
-    public clientMap: { [key: string]: string } = {
-      '=0': 'we do not have any client waiting',
-      '=1': 'we have a client waitng',
-      'other': 'we have # clients waiting'
-    }
+  // i18nPlural
+  public clients: string [] = ['Ana', 'Pedro', 'Diana', 'Juan', 'Doris'];
+
+  public clientMap: { [key: string]: string } = {
+    '=0': 'we do not have any client waiting',
+    '=1': 'we have a client waitng',
+    'other': 'we have # clients waiting'
+  };
+
+  public deleteClient(): void {
+    this.clients.splice(0,1);
+  }
 
 }
