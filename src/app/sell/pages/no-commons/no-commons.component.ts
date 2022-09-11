2a41c337f22321fbe722d2ab80c45ad7920408d2
Emmanuel Tamburini <emmanueltamburini@gmail.com>
2022-09-10 23:24:27 -0400

    i18nSelect pipe has been added

--- src/app/sell/pages/no-commons/no-commons.component.ts
+++ src/app/sell/pages/no-commons/no-commons.component.ts
@@ -1,14 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-no-commons',
   templateUrl: './no-commons.component.html'
 })
-export class NoCommonsComponent implements OnInit {
+export class NoCommonsComponent {
+  public name: string = 'Emmanuel';
+  public gender: string = 'male'
 
-  constructor() { }
-
-  ngOnInit(): void {
+  public mapPronouns: { [key: string]: string } = {
+    'female': 'she',
+    'male': 'he'
   }
 
 }
