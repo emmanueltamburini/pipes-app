6f7c9040b1499d2840881bb10998f134ce16ff3d
Emmanuel Tamburini <emmanueltamburini@gmail.com>
2022-09-11 18:59:27 -0400

    first own pipes view has been added

---
+++ src/app/sell/interfaces/sell.interfaces.ts
@@ -0,0 +1,13 @@
+export enum Color {
+  red,
+  black,
+  blue,
+  green
+}
+
+export interface Heroe {
+  name: string,
+  fly: boolean,
+  color: Color
+}
+
--- src/app/sell/pages/sort/sort.component.ts
+++ src/app/sell/pages/sort/sort.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Color, Heroe } from '../../interfaces/sell.interfaces';
 
 @Component({
   selector: 'app-sort',
@@ -7,6 +8,33 @@ import { Component } from '@angular/core';
 export class SortComponent {
   public value: string = 'Example';
   public booleanValue: boolean = false;
+  public heroes: Heroe[] = [
+    {
+      name: 'Superman',
+      fly: true,
+      color: Color.blue
+    },
+    {
+      name: 'Batman',
+      fly: false,
+      color: Color.black
+    },
+    {
+      name: 'Robin',
+      fly: false,
+      color: Color.green
+    },
+    {
+      name: 'Daredevil',
+      fly: false,
+      color: Color.red
+    },
+    {
+      name: 'Green lantern',
+      fly: true,
+      color: Color.green
+    },
+  ];
 
   public changeBoolean(): void {
     this.booleanValue = !this.booleanValue;
